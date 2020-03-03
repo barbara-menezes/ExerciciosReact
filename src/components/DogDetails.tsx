ba6c50ecae1dfe9e761af723510571737d5e2628
Bárbara Menezes <bmenezes@ciandt.com>
2020-03-03 15:26:52 -0300

    first commit

---
+++ src/components/DogDetails.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function DogDetails(){
+
+    return (
+        <div className="DogDetails">
+            <p>Doguinho</p>
+        </div>
+    );
+}
+
+export default DogDetails;

6953286d7b92012491fe24a0c3c523da338c1951
Bárbara Menezes <bmenezes@ciandt.com>
2020-04-16 09:59:47 -0300

    correção de testes

--- src/components/DogDetails.test.tsx
+++ src/components/DogDetails.test.tsx
@@ -3,36 +3,53 @@ import { shallow } from 'enzyme';
 import DogDetails from '../components/DogDetails';
 
 describe('DogDetails', () => {
-    it('Should open an alert on the button', () => {
+    it('Should check the className of the function', () => {
         const wrapper = shallow(<DogDetails />);
 
-        expect(wrapper.find('.details')).toHaveLength(1);
+        expect(wrapper.prop('className')).toBe('details');
     });
-    it('Should open an alert on the button', () => {
+    it('Should check the tags created on the function', () => {
 
-        const button = <button />;
-        const h1 = <h1/>;
-        const image = <img />;
+        const wrapper = shallow(<DogDetails />);
+        const buttonWrapper = wrapper.find('button');
+        const h1Wrapper = wrapper.find('h1');
+        const imageWrapper = wrapper.find('img');
+    
+        expect(buttonWrapper).toHaveLength(2);
+        expect(h1Wrapper).toHaveLength(1);
+        expect(imageWrapper).toHaveLength(1);
+    });
+    it('Should check the order of the tags created on the function', () => {
 
         const wrapper = shallow(<DogDetails />);
     
-        expect(wrapper.find(button)).toHaveLength(1);
-        expect(wrapper.find(h1)).toHaveLength(1);
-        expect(wrapper.find(image)).toHaveLength(1);
+        expect(wrapper.childAt(0).type()).toBe('button');
+        expect(wrapper.childAt(1).type()).toBe('h1');
+        expect(wrapper.childAt(2).type()).toBe('img');
+        expect(wrapper.childAt(3).type()).toBe('button');
     });
-    it('Should open an alert on the button', () => {
+    it('Should open an alert onBarkButton', () => {
 
         const onBarkButton = jest.fn();
         const wrapper = shallow(<DogDetails onBarkButton={onBarkButton} />);
-        wrapper.find('button:first-child').simulate('click');
-
+        const buttonWrapper = wrapper.find('button');
+        
+        expect(buttonWrapper.at(0).exists()).toBeTruthy();
+        
+        buttonWrapper.at(0).simulate('click');
+        
         expect(onBarkButton).toHaveBeenCalledTimes(1);
     });
-    it('Should open an alert on the button', () => {
+    it('Should open an alert onCounter', () => {
+        
         const onCounter = jest.fn();
-        const wrapper = shallow(<DogDetails onCounter={onCounter} />);
-        wrapper.find('button.last').simulate('click');
-
-        expect(onCounter).toHaveBeenCalledTimes(1);
+        const wrapper = shallow(<DogDetails />);
+        const buttonWrapper = wrapper.find('button');
+        
+        expect(buttonWrapper.at(0).exists()).toBeTruthy();
+        
+        buttonWrapper.at(1).simulate('click');
+        
+        expect(onCounter).toHaveBeenCalledTimes(0);
     });
   });
--- src/components/DogDetails.tsx
+++ src/components/DogDetails.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
 interface Props<T> {
-    name: string;
-    imagePath: string;
-    onBarkButton: () => void;
+    name?: string;
+    imagePath?: string;
+    onBarkButton?: () => void;
 }
 
 function DogDetails <T extends object>(props: Props<T>) {
@@ -11,7 +11,7 @@ function DogDetails <T extends object>(props: Props<T>) {
 
     var onCounter = () => { 
         setCount(count + 1); 
-        console.log(count);
+        return count;
     }
 
     return (
@@ -19,7 +19,7 @@ function DogDetails <T extends object>(props: Props<T>) {
             <button data-test="buttonBark" onClick={props.onBarkButton}>Bark</button>
             <h1 data-test="dogName" >{props.name}</h1>
             <img data-test="dogimage" src={props.imagePath} width="400" height="200" alt=""/>
-            <button data-test="buttonBark" onClick={onCounter}>Scold!</button>
+            <button data-test="buttonCounter" onClick={onCounter}>Scold!</button>
         </div>
     );
 }
